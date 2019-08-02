cf6ed8d250ef601b6aa4313f240272ae4eb84fd0
Ady ur Rehman <53148652+AdyRehman@users.noreply.github.com>
2019-08-02 15:06:32 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,6 @@ import numpy as np
 import random
 import io
 
-usr_pws_typ = [['user1', 'pass1', 'normal'], ['user2', 'pass2', 'normal']]
-df = pd.DataFrame(usr_pws_typ, columns=['username', 'password', 'type'])
-
 ############################routing#################
 app = Flask(__name__)
 
@@ -35,12 +32,18 @@ def login(loginErrorStatus):
 def do_admin_login():
     print('third')
     print(request.form['password'], request.form['username'])
+
+    df = pd.read_csv("./static/users.csv")
     user = str(request.form['username'])
     password = str(request.form['password'])
     status = False
     for u in df['username']:
         if u == user :
             status = df['password'][df[df['username'] == user].index.values.astype(int)] == password
+            print(type(status))
+            print(status)
+            status = status.tolist()
+            print(status)
             status = status[0]
             print(status)
     if (status):
@@ -62,6 +65,22 @@ def password():
 @app.route('/signup',methods = ['GET', 'POST'])
 def signup():
     return render_template('signup.html')
+@app.route('/signupf',methods = ['GET', 'POST'])
+def signupf():
+    user = str(request.form.get("username", False))
+    password = str(request.form.get("password", False))
+    typex = str(request.form.get("type", False))
+
+    print(user,typex,password)
+    dfu = pd.read_csv("./static/users.csv")
+    dfu = dfu.append({'username': user, 'password': password, 'type': typex}, ignore_index=True)
+    dfu.to_csv('./static/users.csv', index=False)
+    flash('You were successfully signed up')
+
+    if request.method == 'POST':
+        return render_template('login.html')
+    else:
+        return render_template('login.html')
 
 @app.route("/home")
 def home():
